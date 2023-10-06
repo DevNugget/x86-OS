4c8ee1e20a587a450da8b4693b2d6bb7c4d9b71a
DevNugget <dominicthemiya@gmail.com>
2023-10-06 17:48:00 +0530

    Added IDT wooo

--- idt.c
+++ idt.c
@@ -1,4 +1,5 @@
 #include "include/idt.h"
+#include "include/memory.h"
 
 void idt_set_gate(unsigned char idx, unsigned long base, unsigned short sel, unsigned char flags) {
     idt[idx].base_lo = base & 0b1111111111111111;
@@ -10,8 +11,15 @@ void idt_set_gate(unsigned char idx, unsigned long base, unsigned short sel, uns
 }
 
 void init_idt() {
+    // Sets up pointer to IDT
     idt_ptr.limit = (sizeof(idt_entry_t) * IDT_SIZE) - 1;
     idt_ptr.base = &idt;
 
+    // Clear out IDT, initialize to 0
+    memset(&idt, 0, sizeof(idt_entry_t) * IDT_SIZE); 
 
+    // Add new stuff to IDT here
+    
+    // Set internal register to new IDT created
+    idt_load();
 }
